80ba3b9ec912cf88e46d5736899a42ba22ad08b3
Alikhan <alikhan@MacBook-Pro-Alikhan.local>
2020-02-02 20:21:28 +0600

    CMS-1 add delete capture

--- Dna/Payment/Observer/DataAssignObserver.php
+++ Dna/Payment/Observer/DataAssignObserver.php
@@ -3,7 +3,7 @@
  * Copyright © 2016 Magento. All rights reserved.
  * See COPYING.txt for license details.
  */
-namespace Dna\PaymentObserver;
+namespace Dna\Payment\Observer;
 
 use Magento\Framework\Event\Observer;
 use Magento\Payment\Observer\AbstractDataAssignObserver;
