7fe5625174db41a716a3fbf7f82865dc7f157ebb
Nurbol Sarsenbayev <nurbol@dnapayments.com>
2022-01-18 14:09:32 +0600

    fix error with dnapayments-api-bundle

--- Dna/Payment/view/frontend/requirejs-config.js
+++ Dna/Payment/view/frontend/requirejs-config.js
@@ -1,6 +1,7 @@
 const config = {
     paths: {
-        'dnapayments-api': 'https://pay.dnapayments.com/checkout/payment-api'
+        'dnapayments-api': 'https://pay.dnapayments.com/checkout/payment-api',
+        'dnapayments-api-bundle': 'https://pay.dnapayments.com/checkout/payment-api-bundle',
     },
     map: {
         '*': {
