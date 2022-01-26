69e606689623103b48efa514cf6ff80a244185ca
Nurbol Sarsenbayev <nurbol@dnapayments.com>
2022-01-26 12:31:45 +0600

    remove payment-api-bundle

--- Dna/Payment/view/frontend/requirejs-config.js
+++ Dna/Payment/view/frontend/requirejs-config.js
@@ -1,7 +1,6 @@
 const config = {
     paths: {
-        'dnapayments-api': 'https://pay.dnapayments.com/checkout/payment-api',
-        'dnapayments-api-bundle': 'https://pay.dnapayments.com/checkout/payment-api-bundle',
+        'dnapayments-api': 'https://pay.dnapayments.com/checkout/payment-api'
     },
     map: {
         '*': {
