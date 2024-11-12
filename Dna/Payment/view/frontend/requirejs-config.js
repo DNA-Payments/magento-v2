93b63a95ce92014e397ef49b2f78967d430d9060
Jamshed Nasridinov <jnasridinov@gmail.com>
2024-11-12 11:06:58 +0000

    alipay integration

--- Dna/Payment/view/frontend/requirejs-config.js
+++ Dna/Payment/view/frontend/requirejs-config.js
@@ -2,7 +2,8 @@ const config = {
     paths: {
         'dnapayments-api': 'https://pay.dnapayments.com/checkout/payment-api',
         'dna-google-pay': 'https://pay.dnapayments.com/components/google-pay/google-pay-component',
-        'dna-apple-pay': 'https://pay.dnapayments.com/components/apple-pay/apple-pay-component'
+        'dna-apple-pay': 'https://pay.dnapayments.com/components/apple-pay/apple-pay-component',
+        'dna-alipay-plus': 'https://test-pay.dnapayments.com/components/alipay-wechat-pay/alipay-wechat-pay-component'
     },
     map: {
         '*': {
